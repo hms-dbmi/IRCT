778df549fc42d7788a78df1efa6355ca6c69d305
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2016-04-21 11:29:36 -0400

    Added new converters

---
+++ IRCT-API/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/dataconverter/XSLXTabularDataConverter.java
@@ -0,0 +1,97 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package edu.harvard.hms.dbmi.bd2k.irct.dataconverter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.StreamingOutput;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.Result;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.ResultDataType;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.exception.PersistableException;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.exception.ResultSetException;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.tabular.ResultSet;
+
+public class XSLXTabularDataConverter implements ResultDataConverter {
+
+	@Override
+	public ResultDataType getResultDataType() {
+		return ResultDataType.TABULAR;
+	}
+	
+	@Override
+	public String getFileExtension() {
+		return ".xlsx";
+	}
+
+	@Override
+	public String getName() {
+		return "XLSX";
+	}
+
+	@Override
+	public String getMediaType() {
+		return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+	}
+
+	@Override
+	public StreamingOutput createStream(final Result result) {
+		StreamingOutput stream = new StreamingOutput() {
+			@Override
+			public void write(OutputStream outputStream) throws IOException,
+					WebApplicationException {
+				try {
+					ResultSet rs = (ResultSet) result.getData();
+					rs.load(result.getResultSetLocation());
+					
+					SXSSFWorkbook wb = new SXSSFWorkbook(100);
+					// Create Sheet
+					Sheet sh = wb.createSheet("Results");
+
+					// Create Header
+					CellStyle headerStyle = wb.createCellStyle();
+					Font font = wb.createFont();
+					font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+					headerStyle.setFont(font);
+
+					Row headerRow = sh.createRow(0);
+					for (int i = 0; i < rs.getColumnSize(); i++) {
+						Cell cell = headerRow.createCell(i);
+						cell.setCellStyle(headerStyle);
+						cell.setCellValue(rs.getColumn(i).getName());
+					}
+
+					// Add data
+					rs.beforeFirst();
+					int rowNum = 1;
+					while (rs.next()) {
+						Row row = sh.createRow(rowNum);
+						for (int i = 0; i < rs.getColumnSize(); i++) {
+							Cell cell = row.createCell(i);
+							cell.setCellValue(rs.getString(i));
+						}
+						rowNum++;
+					}
+					wb.write(outputStream);
+					wb.close();
+					
+				} catch (ResultSetException | PersistableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				outputStream.close();
+			}
+		};
+		return stream;
+	}
+}
