e0bb8cccaf625cb5efe77de39e5d5bb2c33cf52b
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2016-07-26 10:35:08 -0400

    Initial Commit

---
+++ IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/aws/event/result/S3AfterSaveResult.java
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package edu.harvard.hms.dbmi.bd2k.irct.aws.event.result;
+
+import edu.harvard.hms.dbmi.bd2k.irct.event.result.AfterSaveResult;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.Result;
+
+public class S3AfterSaveResult implements AfterSaveResult {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fire(Result result) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/ws/rs/resultconverter/CSVTabularDataConverter.java
@@ -0,0 +1,95 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package edu.harvard.hms.dbmi.bd2k.irct.ws.rs.resultconverter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.StreamingOutput;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import edu.harvard.hms.dbmi.bd2k.irct.dataconverter.ResultDataConverter;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.Result;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.ResultDataType;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.exception.PersistableException;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.exception.ResultSetException;
+import edu.harvard.hms.dbmi.bd2k.irct.model.result.tabular.ResultSet;
+
+/**
+ * A data converter that returns a CSV Stream
+ * 
+ * @author Jeremy R. Easton-Marks
+ *
+ */
+public class CSVTabularDataConverter implements ResultDataConverter {
+
+	@Inject
+	Logger log;
+	
+	@Override
+	public ResultDataType getResultDataType() {
+		return ResultDataType.TABULAR;
+	}
+	
+	@Override
+	public String getFileExtension() {
+		return ".csv";
+	}
+
+	@Override
+	public String getName() {
+		return "CSV";
+	}
+
+	@Override
+	public String getMediaType() {
+		return "text/csv";
+	}
+
+	@Override
+	public StreamingOutput createStream(final Result result) {
+		StreamingOutput stream = new StreamingOutput() {
+			@Override
+			public void write(OutputStream outputStream) throws IOException,
+					WebApplicationException {
+				try {
+					ResultSet rs = (ResultSet) result.getData();
+					rs.load(result.getResultSetLocation());
+					
+					CSVPrinter printer = new CSVPrinter(new OutputStreamWriter(
+							outputStream), CSVFormat.DEFAULT);
+
+					String[] columnHeaders = new String[rs.getColumnSize()];
+					for (int i = 0; i < rs.getColumnSize(); i++) {
+						columnHeaders[i] = rs.getColumn(i).getName();
+					}
+					printer.printRecord((Object[]) columnHeaders);
+
+					rs.beforeFirst();
+					while (rs.next()) {
+						String[] row = new String[rs.getColumnSize()];
+						for (int i = 0; i < rs.getColumnSize(); i++) {
+							row[i] = rs.getString(i);
+						}
+						printer.printRecord((Object[]) row);
+					}
+
+					printer.flush();
+					printer.close();
+					
+				} catch (ResultSetException | PersistableException e) {
+					log.info("Error creating CSV Stream: " + e.getMessage());
+				}
+				outputStream.close();
+			}
+		};
+		return stream;
+	}
+}
