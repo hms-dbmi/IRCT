7bd5c006bf8586e45f0301c33c55f30a72495bb8
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2016-07-29 08:27:11 -0400

    Test log
    Test retrieving log from context listener

--- IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/monitoring/event/action/MonitoringBeforeQuery.java
+++ IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/monitoring/event/action/MonitoringBeforeQuery.java
@@ -4,6 +4,12 @@
 package edu.harvard.hms.dbmi.bd2k.irct.monitoring.event.action;
 
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.commons.logging.Log;
+
+import edu.harvard.hms.dbmi.bd2k.irct.controller.ResultController;
 import edu.harvard.hms.dbmi.bd2k.irct.event.action.BeforeQuery;
 import edu.harvard.hms.dbmi.bd2k.irct.model.query.Query;
 import edu.harvard.hms.dbmi.bd2k.irct.model.resource.Resource;
@@ -11,15 +17,26 @@ import edu.harvard.hms.dbmi.bd2k.irct.model.security.SecureSession;
 
 public class MonitoringBeforeQuery implements BeforeQuery {
 	
+	private Log log;
+	
 	public String getName() {
 		return null;
 	}
 
 	public void init() {
+		InitialContext ic;
+		try {
+			ic = new InitialContext();
+			this.log = (Log) ic.lookup("java:module/Log");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	public void fire(SecureSession session, Resource resource, Query query) {
 		System.out.println("QUERY: " + session.getUser().getName() + " : " + resource.getName() + " : " + query.toString());
+		System.out.println(log == null);
 	}
 	
 	
