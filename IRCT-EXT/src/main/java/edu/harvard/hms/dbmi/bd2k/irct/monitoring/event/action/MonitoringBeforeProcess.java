79d6b4cef8ce38f2a607e7c51a246434ce4acf34
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2016-07-29 08:54:43 -0400

    Rolling Back
    Rolling Back to System.out

--- IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/monitoring/event/action/MonitoringBeforeProcess.java
+++ IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/monitoring/event/action/MonitoringBeforeProcess.java
@@ -3,10 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package edu.harvard.hms.dbmi.bd2k.irct.monitoring.event.action;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
-import org.apache.commons.logging.Log;
 
 import edu.harvard.hms.dbmi.bd2k.irct.event.action.BeforeProcess;
 import edu.harvard.hms.dbmi.bd2k.irct.model.process.IRCTProcess;
@@ -14,25 +10,18 @@ import edu.harvard.hms.dbmi.bd2k.irct.model.security.SecureSession;
 
 public class MonitoringBeforeProcess implements BeforeProcess {
 	
-	private Log log;
 	
 	public String getName() {
 		return null;
 	}
 
 	public void init() {
-		InitialContext ic;
-		try {
-			ic = new InitialContext();
-			this.log = (Log) ic.lookup("java:module/Log");
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
+	
 	}
 
 	@Override
 	public void fire(SecureSession session, IRCTProcess process) {
-		log.info("PROCESS: " + session.getUser().getName() + " : " + process.toString());
+		System.out.println("PROCESS: " + session.getUser().getName() + " : " + process.toString());
 		
 	}
 	
--- IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/monitoring/event/action/MonitoringBeforeQuery.java
+++ IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/monitoring/event/action/MonitoringBeforeQuery.java
@@ -4,10 +4,6 @@
 package edu.harvard.hms.dbmi.bd2k.irct.monitoring.event.action;
 
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
-import org.apache.commons.logging.Log;
 
 import edu.harvard.hms.dbmi.bd2k.irct.event.action.BeforeQuery;
 import edu.harvard.hms.dbmi.bd2k.irct.model.query.Query;
@@ -16,25 +12,16 @@ import edu.harvard.hms.dbmi.bd2k.irct.model.security.SecureSession;
 
 public class MonitoringBeforeQuery implements BeforeQuery {
 	
-	private Log log;
 	
 	public String getName() {
 		return null;
 	}
 
 	public void init() {
-		InitialContext ic;
-		try {
-			ic = new InitialContext();
-			this.log = (Log) ic.lookup("java:module/Log");
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
-		
 	}
 
 	public void fire(SecureSession session, Resource resource, Query query) {
-		log.info("QUERY: " + session.getUser().getName() + " : " + resource.getName() + " : " + query.toString());
+		System.out.println("QUERY: " + session.getUser().getName() + " : " + resource.getName() + " : " + query.toString());
 	}
 	
 	
