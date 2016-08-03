3f632e28590fe83910d76967082d9a5a73f325da
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2016-08-03 07:05:58 -0400

    Trying out instance profile credentials
    Trying out instance profile credentials

--- IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/aws/event/result/S3AfterGetResult.java
+++ IRCT-EXT/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/aws/event/result/S3AfterGetResult.java
@@ -12,6 +12,7 @@ import org.apache.commons.logging.LogFactory;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -38,7 +39,8 @@ public class S3AfterGetResult implements AfterGetResult {
 		
 //		BasicAWSCredentials awsCreds = new BasicAWSCredentials(parameters.get("accessId"), parameters.get("accessKey"));
 //		s3client = new AmazonS3Client(awsCreds);
-		s3client = new AmazonS3Client();
+//		s3client = new AmazonS3Client();
+		s3client = new AmazonS3Client(new InstanceProfileCredentialsProvider());
 //		s3client = new AmazonS3Client(new ProfileCredentialsProvider());
 		
 	}
