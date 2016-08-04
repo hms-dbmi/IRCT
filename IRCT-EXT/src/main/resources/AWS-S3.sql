e172a17fbe1003aac4845610682a41aaef708b31
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2016-08-04 12:58:25 -0400

    Generalize S3 SQL Script
    Generalize S3 SQL Script

--- IRCT-EXT/src/main/resources/AWS-S3.sql
+++ IRCT-EXT/src/main/resources/AWS-S3.sql
@@ -4,11 +4,11 @@ set @S3AfterSaveResultId = @S3BeforeGetResultId + 1;
 insert into EventConverterImplementation(id, eventListener, name) values(@S3BeforeGetResultId,'edu.harvard.hms.dbmi.bd2k.irct.aws.event.result.S3AfterGetResult','S3 After Get Result');
 insert into EventConverterImplementation(id, eventListener, name) values(@S3AfterSaveResultId,'edu.harvard.hms.dbmi.bd2k.irct.aws.event.result.S3AfterSaveResult','S3 After Save Result');
 
-insert into event_parameters(id, name, value) values(@S3BeforeGetResultId, 'Bucket Name', 'dbmi-avillachlab-data');
-insert into event_parameters(id, name, value) values(@S3AfterSaveResultId, 'Bucket Name', 'dbmi-avillachlab-data');
+insert into event_parameters(id, name, value) values(@S3BeforeGetResultId, 'Bucket Name', 'Bucket Name');
+insert into event_parameters(id, name, value) values(@S3AfterSaveResultId, 'Bucket Name', 'Bucket Name');
 
-insert into event_parameters(id, name, value) values(@S3BeforeGetResultId, 'resultDataFolder', 'dbmi-avillachlab-data');
-insert into event_parameters(id, name, value) values(@S3AfterSaveResultId, 'resultDataFolder', 'dbmi-avillachlab-data');
+insert into event_parameters(id, name, value) values(@S3BeforeGetResultId, 'resultDataFolder', 'Local Data Folder');
+insert into event_parameters(id, name, value) values(@S3AfterSaveResultId, 'resultDataFolder', 'Local Data Folder');
 
-insert into event_parameters(id, name, value) values(@S3BeforeGetResultId, 's3Folder', 'tmp/IRCT/je72Local/result/');
-insert into event_parameters(id, name, value) values(@S3AfterSaveResultId, 's3Folder', 'tmp/IRCT/je72Local/result/');
+insert into event_parameters(id, name, value) values(@S3BeforeGetResultId, 's3Folder', 'S3 Data Folder');
+insert into event_parameters(id, name, value) values(@S3AfterSaveResultId, 's3Folder', 'S3 Data Folder');
