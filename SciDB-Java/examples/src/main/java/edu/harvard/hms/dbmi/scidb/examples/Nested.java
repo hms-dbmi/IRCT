df1276ad80d01a0d864a6c5f0dd935ca0e8591c5
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2016-11-14 09:34:59 -0500

    Clean Up Example
    Clean up an example

--- SciDB-Java/examples/src/main/java/edu/harvard/hms/dbmi/scidb/examples/Nested.java
+++ SciDB-Java/examples/src/main/java/edu/harvard/hms/dbmi/scidb/examples/Nested.java
@@ -1,17 +1,13 @@
 package edu.harvard.hms.dbmi.scidb.examples;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.nio.charset.Charset;
-
-import org.apache.commons.io.IOUtils;
+import java.io.InputStreamReader;
 
 import edu.harvard.hms.dbmi.scidb.SciDB;
 import edu.harvard.hms.dbmi.scidb.SciDBArray;
 import edu.harvard.hms.dbmi.scidb.SciDBFilterFactory;
 import edu.harvard.hms.dbmi.scidb.SciDBFunction;
-import edu.harvard.hms.dbmi.scidb.SciDBFunctionFactory;
 import edu.harvard.hms.dbmi.scidb.exception.NotConnectedException;
 
 public class Nested {
@@ -20,7 +16,6 @@ public class Nested {
 		long startTime = System.currentTimeMillis();
 		System.out.println("Nested Example");
 		SciDB sciDB = new SciDB();
-		sciDB.connect("http://192.168.56.101:8080");
 
 		String arrayName = "IHI_ACCELEROMETER";
 
@@ -31,23 +26,18 @@ public class Nested {
 					sciDB.project(sciDB.filter(sciDB.filter(new SciDBArray(arrayName), whereOperation), whereOperation2),
 					"acc_x", "acc_y", "acc_z"), "dcsv");
 			System.out.println("Query Id: " + queryId);
-			String[] rawLines = inputStreamToString(sciDB.readLines()).split("\n");
-			for(int i = 0; i <= 25; i++) {
-				System.out.println(rawLines[i]);
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(sciDB.readLines()));
+			String line = null;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
 			}
+			
 		} catch (NotConnectedException | IOException e) {
 			e.printStackTrace();
 		}
 		long stopTime = System.currentTimeMillis();
 		System.out.println("Time: " + (stopTime - startTime));
 		sciDB.close();
-	}
-
-	private static String inputStreamToString(InputStream inputStream)
-			throws IOException {
-		StringWriter writer = new StringWriter();
-		IOUtils.copy(inputStream, writer, Charset.defaultCharset());
-		inputStream.close();
-		return writer.toString();
 	}
 }
