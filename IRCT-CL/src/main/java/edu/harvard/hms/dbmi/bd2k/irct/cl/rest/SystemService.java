ade9429bfabae606e14d2d6572cc0531812cba2d
Gabor Korodi <gabor_korodi@hms.harvard.edu>
2017-04-06 15:15:29 -0400

    DI-765 Add /about endpoint to CL module
    To be able to query the currently running application for certain
    system settings, create a new “systemService/about” URL, that returns a
    JSON Array of those key/value pairs.

--- IRCT-CL/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/cl/rest/SystemService.java
+++ IRCT-CL/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/cl/rest/SystemService.java
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package edu.harvard.hms.dbmi.bd2k.irct.cl.rest;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
@@ -10,16 +11,19 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonStructure;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import edu.harvard.hms.dbmi.bd2k.irct.IRCTApplication;
 import edu.harvard.hms.dbmi.bd2k.irct.model.resource.PrimitiveDataType;
+import edu.harvard.hms.dbmi.bd2k.irct.model.security.User;
 
 /**
  * Creates a REST interface for the systems service.
- * 
+ *
  * @author Jeremy R. Easton-Marks
  *
  */
@@ -29,10 +33,13 @@ public class SystemService {
 
 	@Inject
 	Logger log;
-	
+
+	@Inject
+	private HttpSession session;
+
 	/**
 	 * Returns a JSON Array of supported Data Types by the IRCT core.
-	 * 
+	 *
 	 * @return JSON Array of data types
 	 */
 	@GET
@@ -43,6 +50,26 @@ public class SystemService {
 		for (PrimitiveDataType pt : PrimitiveDataType.values()) {
 			build.add(pt.toJson());
 		}
+		return build.build();
+	}
+
+	/**
+	 * Returns a JSON Array of application settings
+	 *
+	 * @return JSON Array of settings (key/value pair)
+	 */
+	@GET
+	@Path("/about")
+	@Produces(MediaType.APPLICATION_JSON)
+	public JsonStructure about() {
+		log.log(Level.FINE, "/about URL");
+
+		IRCTApplication app = new IRCTApplication();
+		JsonArrayBuilder build = Json.createArrayBuilder();
+		build.add(Json.createObjectBuilder().add("version", app.getVersion()));
+		User user = (User) session.getAttribute("user");
+		build.add(Json.createObjectBuilder().add("username", user.getName()));
+		build.add(Json.createObjectBuilder().add("userid", user.getUserId()));
 		return build.build();
 	}
 }
