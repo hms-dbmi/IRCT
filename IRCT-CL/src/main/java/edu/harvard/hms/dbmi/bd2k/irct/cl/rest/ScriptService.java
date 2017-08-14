697af718f3c7aeaa8130cfb20e9bf2c9a89d6553
Jason Stedman <namdets@gmail.com>
2017-08-14 10:35:29 -0400

    a start at submitting queries from ScriptService

--- IRCT-CL/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/cl/rest/ScriptService.java
+++ IRCT-CL/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/cl/rest/ScriptService.java
@@ -2,6 +2,7 @@ package edu.harvard.hms.dbmi.bd2k.irct.cl.rest;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -12,18 +13,30 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import edu.harvard.hms.dbmi.bd2k.irct.model.script.IRCTQuery;
 import edu.harvard.hms.dbmi.bd2k.irct.model.script.ScriptedQuery;
 
 @Path("/script")
 public class ScriptService {
 	
+	@Inject
+	private QueryService queryService;
+	
+	// TODO : This is only required because the IRCT does not properly use the JaxRS providers framework to handle serialization.
+	private ObjectMapper mapper = new ObjectMapper();
+		
 	private Logger logger = Logger.getLogger(getClass());
 		
 	@POST
-	public Response submitScriptQuery(String scriptedQuery) throws JsonParseException, JsonMappingException, IOException{
-		ObjectMapper mapper = new ObjectMapper();
+	public Response submitScriptQuery(String scriptedQueryJson) throws JsonParseException, JsonMappingException, IOException{
+		logger.error(scriptedQueryJson);
+		ScriptedQuery scriptedQuery = mapper.readValue(scriptedQueryJson, ScriptedQuery.class);
 		logger.error(scriptedQuery);
-		logger.error(mapper.readValue(scriptedQuery, ScriptedQuery.class));
+		logger.error(queryService);
+		for(IRCTQuery q : scriptedQuery.getQueries()) {
+			Response response = queryService.runQuery(mapper.writeValueAsString(q));
+			logger.error(response.getEntity());
+		}
 		return Response.ok().build();
 	}
 }
