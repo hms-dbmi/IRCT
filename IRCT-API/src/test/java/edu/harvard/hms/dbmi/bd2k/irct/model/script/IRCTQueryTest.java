c5cade41ffcfea87a202cb3754f73124ba6f8d66
Jason Stedman <jason_stedman@hms.harvard.edu>
2017-08-18 15:00:01 -0400

    AFLQuery serialization example and test

--- IRCT-API/src/test/java/edu/harvard/hms/dbmi/bd2k/irct/model/script/IRCTQueryTest.java
+++ IRCT-API/src/test/java/edu/harvard/hms/dbmi/bd2k/irct/model/script/IRCTQueryTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.util.IOUtils;
 import org.junit.Test;
@@ -12,12 +14,35 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class IRCTQueryTest {
 
-	@Test
-	public void testSerializationSupport() throws IOException {
+	@SuppressWarnings("unchecked")
+	private void testSerialization(String path, Class type) throws IOException{
 		ObjectMapper mapper = new ObjectMapper();
-		InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("query_json/i2b2_tranSmart_query_1.json");
+		InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(path);
 		String json = new String(IOUtils.toByteArray(resourceAsStream));
-		mapper.readValue(json, ScriptedQuery.class);
+		mapper.readValue(json, type);
+	}
+	
+	@Test
+	public void testIRCTQuerySerializationSupport() throws IOException {
+		testSerialization("query_json/i2b2_tranSmart_query_1.json", ScriptedQuery.class);
 	}
 
+	@Test
+	public void testAFLQuerySerializationSupport() throws IOException {
+		testSerialization("query_json/AFLQuery1.json", AFLQuery.class);
+	}
+
+	
+	@Test
+	public void testAFLQuerySerializationSupportBothWays() throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		AFLQuery query = new AFLQuery();
+		List<Where> whereClause = new ArrayList<Where>();
+		whereClause.add(new Where().setDataType("AFL").setSrc("SOME AFL"));
+		query.setWhere(whereClause);
+		String json = mapper.writeValueAsString(query);
+		System.out.println(json);
+		assertEquals(query, mapper.readValue(json, AFLQuery.class));		
+	}
+	
 }
