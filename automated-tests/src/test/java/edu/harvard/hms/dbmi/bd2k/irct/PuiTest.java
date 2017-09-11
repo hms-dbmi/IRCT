f0934597b5efab5a48c8e72f5e7315fe92102011
Jason Stedman <jason_stedman@hms.harvard.edu>
2017-09-11 08:38:29 -0400

    added module for automated tests

---
+++ automated-tests/src/test/java/edu/harvard/hms/dbmi/bd2k/irct/PuiTest.java
@@ -0,0 +1,14 @@
+package edu.harvard.hms.dbmi.bd2k.irct;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PuiTest {
+    
+	@Test
+    public void placeholderTest()
+    {
+        assertTrue( true );
+    }
+}
