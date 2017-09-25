b8ea71b6ca1b3b4e86239fecf0350b9d490077e5
Gabor Korodi <gabor_korodi@hms.harvard.edu>
2017-09-25 18:08:05 -0400

    Logging change

--- IRCT-API/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/security/SecurityUtility.java
+++ IRCT-API/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/security/SecurityUtility.java
@@ -3,7 +3,6 @@ package edu.harvard.hms.dbmi.bd2k.irct.security;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -16,6 +15,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.log4j.Logger;
 
 import edu.harvard.hms.dbmi.bd2k.irct.model.security.JWT;
 import edu.harvard.hms.dbmi.bd2k.irct.model.security.SecureSession;
@@ -28,7 +28,6 @@ import edu.harvard.hms.dbmi.bd2k.irct.model.security.SecureSession;
  *
  */
 public class SecurityUtility {
-
 	/**
 	 * Returns a delegated a token for a user for a new Auth0 application. If an
 	 * error occurs then NULL is returned.
@@ -43,63 +42,61 @@ public class SecurityUtility {
 	 */
 	@Deprecated
 	public static String delegateToken(String namespace,
+
 			String resourceClientId, SecureSession session) {
 
 		if (session.getToken() != null) {
-			java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() returning token stored in the session:"+session.getToken().toString());
+			Logger.getRootLogger()
+					.debug("delegateToken() returning token stored in the session:" + session.getToken().toString());
 			return session.getToken().toString();
 		} else {
-			java.util.logging.Logger.getGlobal().log(Level.SEVERE, "delegateToken() session DOES NOT CONTAINT A TOKEN!");
+			Logger.getRootLogger().debug("delegateToken() session DOES NOT CONTAINT A TOKEN!");
 		}
 
-		java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() namespace:"+namespace+" resource.client.id:"+resourceClientId+" session.id:"+session.getId());
+		Logger.getRootLogger().debug("delegateToken() namespace:" + namespace + " resource.client.id:"
+				+ resourceClientId + " session.id:" + session.getId());
 
 		if (((JWT) session.getToken()).getClientId().equals(resourceClientId)) {
-			java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() session token matches the specified resourceClientId");
+			Logger.getRootLogger().debug("delegateToken() session token matches the specified resourceClientId");
 			return session.getToken().toString();
 		} else {
-			java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() session token DOES NOT MATCH the specified resourceClientId");
+			Logger.getRootLogger().debug("delegateToken() session token DOES NOT MATCH the specified resourceClientId");
 		}
 
 		if (session.getDelegated().containsKey(resourceClientId)) {
-			java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() returning a Bearer token");
-			return "Bearer "
-					+ session.getDelegated().get(resourceClientId).toString();
+			Logger.getRootLogger().debug("delegateToken() returning a Bearer token");
+			return "Bearer " + session.getDelegated().get(resourceClientId).toString();
 		} else {
-			java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() session does NOT have a delegated token.");
+			Logger.getRootLogger().debug("delegateToken() session does NOT have a delegated token.");
 		}
 
 		try {
 			HttpClient client = HttpClientBuilder.create().build();
 			HttpPost post = new HttpPost("https://" + namespace + "/delegation");
-			java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() call to ```https://"+namespace+"/delegation``` URL.");
+			Logger.getRootLogger().debug("delegateToken() call to ```https://" + namespace + "/delegation``` URL.");
 
 			List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-			urlParameters.add(new BasicNameValuePair("grant_type",
-					"urn:ietf:params:oauth:grant-type:jwt-bearer"));
-			urlParameters
-					.add(new BasicNameValuePair("target", resourceClientId));
+			urlParameters.add(new BasicNameValuePair("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer"));
+			urlParameters.add(new BasicNameValuePair("target", resourceClientId));
 
 			//
-			urlParameters.add(new BasicNameValuePair("client_id",
-					((JWT) session.getToken()).getClientId()));
-			urlParameters.add(new BasicNameValuePair("scope",
-					"openid name email"));
+			urlParameters.add(new BasicNameValuePair("client_id", ((JWT) session.getToken()).getClientId()));
+			urlParameters.add(new BasicNameValuePair("scope", "openid name email"));
 			urlParameters.add(new BasicNameValuePair("api_type", "app"));
-			urlParameters.add(new BasicNameValuePair("id_token", session
-					.getToken().toString().split(" ")[1]));
+			urlParameters.add(new BasicNameValuePair("id_token", session.getToken().toString().split(" ")[1]));
 
 			post.setEntity(new UrlEncodedFormEntity(urlParameters));
 
 			HttpResponse response = client.execute(post);
-			JsonReader reader = Json.createReader(response.getEntity()
-					.getContent());
+			JsonReader reader = Json.createReader(response.getEntity().getContent());
 
 			JsonObject responseObject = reader.readObject();
 
-			if(responseObject.containsKey("error")) {
-				java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() Could not get delegated token. "+responseObject.toString());
-				java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() returning NULL, after error response from delegation authority");
+			if (responseObject.containsKey("error")) {
+				Logger.getRootLogger()
+						.debug("delegateToken() Could not get delegated token. " + responseObject.toString());
+				Logger.getRootLogger()
+						.debug("delegateToken() returning NULL, after error response from delegation authority");
 				return null;
 			}
 
@@ -107,15 +104,16 @@ public class SecurityUtility {
 			jwt.setType(responseObject.getString("token_type"));
 			jwt.setIdToken(responseObject.getString("id_token"));
 			session.getDelegated().put(resourceClientId, jwt);
-			java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() returning new delegation token:"+responseObject.getString("id_token"));
+			Logger.getRootLogger()
+					.debug("delegateToken() returning new delegation token:" + responseObject.getString("id_token"));
 			return "Bearer " + responseObject.getString("id_token");
 
 		} catch (IOException e) {
-			java.util.logging.Logger.getGlobal().log(Level.SEVERE, "delegateToken() Exception:"+e.getMessage());
+			Logger.getRootLogger().error("delegateToken() Exception:" + e.getMessage());
 			e.printStackTrace();
 		}
 
-		java.util.logging.Logger.getGlobal().log(Level.FINE, "delegateToken() returning NULL, after exception.");
+		Logger.getRootLogger().debug("delegateToken() returning NULL, after exception.");
 		return null;
 	}
 }
