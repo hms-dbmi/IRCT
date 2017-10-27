ecdacd6626c2780f44632f2457437e05b9f04d40
Gabor Korodi <gabor_korodi@hms.harvard.edu>
2017-10-27 10:00:14 -0400

    Fix error response from Results endpoint
    Add try-catch around every endpoint processing, so all exceptions returned will be handled and only JSON gets returned.

--- IRCT-CL/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/cl/rest/ResultService.java
+++ IRCT-CL/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/cl/rest/ResultService.java
@@ -4,8 +4,6 @@
 package edu.harvard.hms.dbmi.bd2k.irct.cl.rest;
 
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
@@ -23,31 +21,28 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
+
 import edu.harvard.hms.dbmi.bd2k.irct.controller.ResultController;
 import edu.harvard.hms.dbmi.bd2k.irct.dataconverter.ResultDataStream;
 import edu.harvard.hms.dbmi.bd2k.irct.model.result.Result;
-import edu.harvard.hms.dbmi.bd2k.irct.model.result.ResultStatus;
 import edu.harvard.hms.dbmi.bd2k.irct.model.security.User;
 
 /**
  * Creates a REST interface for the result service
- * 
- * @author Jeremy R. Easton-Marks
- *
  */
 @Path("/resultService")
 @RequestScoped
 @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 public class ResultService {
-	
+
 	@Inject
 	private ResultController rc;
 
 	@Inject
 	private HttpSession session;
-	
-	@Inject
-	private Logger logger;
+
+	private Logger logger = Logger.getLogger(this.getClass());
 
 	/**
 	 * Returns a list of available results for that user
@@ -59,18 +54,17 @@ public class ResultService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response availableResults() {
 		JsonArrayBuilder response = Json.createArrayBuilder();
-		User user = (User) session.getAttribute("user");
-		List<Result> availableResults = rc.getAvailableResults(user);
+		try {
+			User user = (User) session.getAttribute("user");
 
-		for (Result result : availableResults) {
-			JsonObjectBuilder resultJSON = Json.createObjectBuilder();
-			resultJSON.add("resultId", result.getId());
-			resultJSON.add("status", result.getResultStatus().toString());
-			response.add(resultJSON.build());
+			for (Result result : rc.getAvailableResults(user)) {
+				response.add(Json.createObjectBuilder().add("status", result.getResultStatus().toString())
+						.add("resultId", result.getId()));
+			}
+		} catch (Exception e) {
+			response.add(Json.createObjectBuilder().add("status", "error").add("message", e.getMessage()));
 		}
-
-		return Response.ok(response.build(), MediaType.APPLICATION_JSON)
-				.build();
+		return Response.ok(response.build(), MediaType.APPLICATION_JSON).build();
 	}
 
 	/**
@@ -85,21 +79,60 @@ public class ResultService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response resultStatus(@PathParam("resultId") Long resultId) {
 		JsonObjectBuilder response = Json.createObjectBuilder();
-		User user = (User) session.getAttribute("user");
-
-		Result result = rc.getResult(user, resultId);
-		if (result == null) {
-			response.add("message", "Unable to get result for that id");
-		} else {
-			response.add("resultId", resultId);
-			response.add("status", result.getResultStatus().toString());
-			if(result.getResultStatus() == ResultStatus.ERROR) {
+		try {
+			User user = (User) session.getAttribute("user");
+			Result result = rc.getResult(user, resultId);
+			if (result == null) {
+				response.add("message", "Unable to get result for that id");
+			} else {
+				response.add("resultId", resultId);
+				response.add("status", result.getResultStatus().toString());
 				response.add("message", result.getMessage());
 			}
+		} catch (Exception e) {
+			response.add("status", "error");
+			response.add("message", e.getMessage());
 		}
 
-		return Response.ok(response.build(), MediaType.APPLICATION_JSON)
-				.build();
+		return Response.ok(response.build(), MediaType.APPLICATION_JSON).build();
+	}
+
+	/**
+	 * Returns the full status of a result
+	 * 
+	 * @param resultId
+	 *            Id of the result
+	 * @return Status details of the result
+	 */
+	@GET
+	@Path("/results/{resultId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response results(@PathParam("resultId") Long resultId) {
+		JsonObjectBuilder response = Json.createObjectBuilder();
+		try {
+			User user = (User) session.getAttribute("user");
+			response.add("user",
+					Json.createObjectBuilder().add("name", user.getName()).add("userId", user.getUserId()));
+
+			Result result = rc.getResult(user, resultId);
+			if (result == null) {
+				response.add("message", "Unable to get result for that id");
+			} else {
+				response.add("resultId", resultId);
+				response.add("resultStatus", result.getResultStatus().toString());
+				response.add("jobType", result.getJobType());
+				response.add("resourceActionId", result.getResourceActionId());
+				response.add("resultId", result.getResultSetLocation());
+				response.add("startTime", result.getStartTime().toString());
+				response.add("endTime", result.getEndTime().toString());
+				response.add("message", result.getMessage());
+			}
+		} catch (Exception e) {
+			response.add("status", "error");
+			response.add("message", e.getMessage());
+		}
+
+		return Response.ok(response.build(), MediaType.APPLICATION_JSON).build();
 	}
 
 	/**
@@ -114,30 +147,29 @@ public class ResultService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response availableFormats(@PathParam("resultId") Long resultId) {
 		JsonArrayBuilder response = Json.createArrayBuilder();
-		User user = (User) session.getAttribute("user");
+		try {
+			User user = (User) session.getAttribute("user");
+			List<String> availableFormats = rc.getAvailableFormats(user, resultId);
+			if (availableFormats == null) {
+				JsonObjectBuilder notFoundResponse = Json.createObjectBuilder();
+				notFoundResponse.add("message", "Unable to get available formats for that id");
+				return Response.ok(notFoundResponse.build(), MediaType.APPLICATION_JSON).build();
+			}
 
-		List<String> availableFormats = rc.getAvailableFormats(user, resultId);
-
-		if (availableFormats == null) {
-			JsonObjectBuilder notFoundResponse = Json.createObjectBuilder();
-			notFoundResponse.add("message",
-					"Unable to get available formats for that id");
-			return Response.ok(notFoundResponse.build(),
-					MediaType.APPLICATION_JSON).build();
+			for (String availableFormat : availableFormats) {
+				response.add(availableFormat);
+			}
+		} catch (Exception e) {
+			response.add(Json.createObjectBuilder().add("status", "error").add("message", e.getMessage()));
 		}
 
-		for (String availableFormat : availableFormats) {
-			response.add(availableFormat);
-		}
-
-		return Response.ok(response.build(), MediaType.APPLICATION_JSON)
-				.build();
+		return Response.ok(response.build(), MediaType.APPLICATION_JSON).build();
 	}
 
 	/**
-	 * Returns a result in the desired format if available. If download is set to
-	 * Yes then the file is returned with Content-Disposition set as attachment
-	 * and a file name.
+	 * Returns a result in the desired format if available. If download is set
+	 * to Yes then the file is returned with Content-Disposition set as
+	 * attachment and a file name.
 	 * 
 	 * @param resultId
 	 *            Result Id
@@ -149,34 +181,37 @@ public class ResultService {
 	 */
 	@GET
 	@Path("/result/{resultId}/{format}")
-	public Response download(@PathParam("resultId") Long resultId,
-			@PathParam("format") String format,
+	public Response download(@PathParam("resultId") Long resultId, @PathParam("format") String format,
 			@QueryParam("download") String download) {
-		
-		logger.log(Level.FINE, "/result format:"+(format==null?"null":format)+" download:"+(download==null?"null":download));
-		User user = (User) session.getAttribute("user");
 
-		ResultDataStream rds = rc.getResultDataStream(user, resultId, format);
-		
-		if ((rds == null) || (rds.getMediaType() == null)) {
-			logger.log(Level.FINE, "/result rds or mediaType is null");
-			
-			JsonObjectBuilder jsonResponse = Json.createObjectBuilder();
-			jsonResponse.add("message", "Unable to retrieve result.");
-			return Response
-					.ok(jsonResponse.build(), MediaType.APPLICATION_JSON)
-					.build();
-		}
+		logger.debug("/result resultId:" + resultId + " format:" + format + " download:" + download);
+		ResultDataStream rds = null;
+		try {
+			User user = (User) session.getAttribute("user");
+			rds = rc.getResultDataStream(user, resultId, format);
 
-		if ((download != null) && (download.equalsIgnoreCase("Yes"))) {
-			logger.log(Level.FINE, "/result initiate download with mediaType:"+rds.getMediaType().toString());
-			return Response
-					.ok(rds.getResult(), rds.getMediaType())
-					.header("Content-Disposition",
-							"attachment; filename=IRCT-" + resultId
-									+ rds.getFileExtension()).build();
+			logger.debug("/result `ResultDataStream` mediatype is " + rds.getMediaType());
+
+			if ((rds == null) || (rds.getMediaType() == null)) {
+				logger.debug("/result rds is null");
+
+				JsonObjectBuilder jsonResponse = Json.createObjectBuilder();
+				jsonResponse.add("message", "Unable to retrieve result.");
+				return Response.ok(jsonResponse.build(), MediaType.APPLICATION_JSON).build();
+			}
+
+			if ((download != null) && (download.equalsIgnoreCase("Yes"))) {
+				logger.debug("/result initiate download with mediaType:" + rds.getMediaType().toString());
+				return Response.ok(rds.getResult(), rds.getMediaType())
+						.header("Content-Disposition", "attachment; filename=IRCT-" + resultId + rds.getFileExtension())
+						.build();
+			}
+
+		} catch (Exception e) {
+			return Response.ok(Json.createObjectBuilder().add("status", "error").add("message", e.getMessage()),
+					MediaType.APPLICATION_JSON).build();
 		}
-		logger.log(Level.FINE, "/result returning");
+		logger.debug("/result returning data");
 		return Response.ok(rds.getResult(), rds.getMediaType()).build();
 	}
 }
