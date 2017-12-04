0c3cd456e34c3cbfb03e5f3e5a98bd5b03d17b49
Gabor Korodi <gabor_korodi@hms.harvard.edu>
2017-12-04 15:14:39 -0500

    Add criteria for picking up only "TREE" resources

--- IRCT-API/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/IRCTApplication.java
+++ IRCT-API/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/IRCTApplication.java
@@ -9,8 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -20,9 +18,12 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.FlushModeType;
+import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+
+import org.apache.log4j.Logger;
 
 import edu.harvard.hms.dbmi.bd2k.irct.dataconverter.ResultDataConverter;
 import edu.harvard.hms.dbmi.bd2k.irct.event.EventConverterImplementation;
@@ -36,9 +37,6 @@ import edu.harvard.hms.dbmi.bd2k.irct.model.result.ResultDataType;
 /**
  * Manages supported resources and join types for this instance of the IRCT
  * application
- *
- * @author Jeremy R. Easton-Marks
- *
  */
 @Startup
 @Singleton
@@ -52,8 +50,7 @@ public class IRCTApplication {
 	private Map<String, IRCTJoin> supportedJoinTypes;
 	private Map<ResultDataType, List<DataConverterImplementation>> resultDataConverters;
 
-	@Inject
-	Logger log;
+	private Logger logger = Logger.getLogger(this.getClass());
 
 	@Inject
 	private EntityManagerFactory objectEntityManager;
@@ -70,27 +67,27 @@ public class IRCTApplication {
 	 */
 	@PostConstruct
 	public void init() {
-		log.info("Starting IRCT Application");
+		logger.info("Starting IRCT Application");
 		this.oem = objectEntityManager.createEntityManager();
 		this.oem.setFlushMode(FlushModeType.COMMIT);
 
-		log.info("Loading Data Converters");
+		logger.info("Loading Data Converters");
 		loadDataConverters();
-		log.info("Finished Data Converters");
+		logger.info("Finished Data Converters");
 
-		log.info("Loading Event Listeners");
+		logger.info("Loading Event Listeners");
 		loadIRCTEventListeners();
-		log.info("Finished Loading Event Listeners");
+		logger.info("Finished Loading Event Listeners");
 
-		log.info("Loading Join Types");
+		logger.info("Loading Join Types");
 		loadJoins();
-		log.info("Finished Loading Join Types");
+		logger.info("Finished Loading Join Types");
 
-		log.info("Loading Resources");
+		logger.info("Loading Resources");
 		loadResources();
-		log.info("Finished Loading Resources");
+		logger.info("Finished Loading Resources");
 
-		log.info("Finished Starting IRCT Application");
+		logger.info("Finished Starting IRCT Application");
 	}
 
 	public String getVersion() {
@@ -109,7 +106,7 @@ public class IRCTApplication {
 				version = p.getProperty("version", "");
 			}
 		} catch (Exception e) {
-			log.log(Level.INFO, "getVersion() ERROR:" + e.getMessage());
+			logger.error("getVersion() Exception:" + e.getMessage());
 		}
 
 		if (version == null) {
@@ -136,7 +133,7 @@ public class IRCTApplication {
 			irctEventListener.registerListener(irctEvent);
 		}
 
-		log.info("Loaded " + allEventListeners.size() + " IRCT Event listeners");
+		logger.info("Loaded " + allEventListeners.size() + " IRCT Event listeners");
 	}
 
 	/**
@@ -161,7 +158,7 @@ public class IRCTApplication {
 
 		}
 
-		log.info("Loaded " + allDCI.size() + " result data converters");
+		logger.info("Loaded " + allDCI.size() + " result data converters");
 	}
 
 	/**
@@ -178,7 +175,7 @@ public class IRCTApplication {
 		for (IRCTJoin jt : oem.createQuery(criteria).getResultList()) {
 			this.supportedJoinTypes.put(jt.getName(), jt);
 		}
-		log.info("Loaded " + this.supportedJoinTypes.size() + " joins");
+		logger.info("Loaded " + this.supportedJoinTypes.size() + " joins");
 	}
 
 	/**
@@ -187,28 +184,25 @@ public class IRCTApplication {
 	 *
 	 */
 	private void loadResources() {
-		log.info("loadResources() Starting");
+		logger.info("loadResources() Starting");
 		setResources(new HashMap<String, Resource>());
 
-		// Run JPA Query to load the resources
-		CriteriaBuilder cb = oem.getCriteriaBuilder();
-		CriteriaQuery<Resource> criteria = cb.createQuery(Resource.class);
-		Root<Resource> load = criteria.from(Resource.class);
-		criteria.select(load);
-		criteria.where(cb.equal(load.get("ontologyType"),"TREE"));
+		// Run JPQL to load the resources
+		Query query = oem.createQuery("SELECT res FROM Resource res WHERE res.ontologyType=:arg1").setParameter("arg1", "TREE");
+		List<Resource> resourceList = query.getResultList();
 
-		for (Resource resource : oem.createQuery(criteria).getResultList()) {
+		for (Resource resource : resourceList) {
 			try {
-				log.info("loadResources() Setting up resource:"+resource.toString()+" "+resource.getId()+" "+resource.getClass().toString());
+				logger.info("loadResources() Setting up resource:"+resource.toString()+" "+resource.getId()+" "+resource.getClass().toString());
 				resource.setup();
-				log.info("loadResources() resource `"+resource.getName()+"` has been loaded");
+				logger.info("loadResources() resource `"+resource.getName()+"` has been loaded");
 				this.resources.put(resource.getName(), resource);
 			} catch (ResourceInterfaceException e) {
-				log.warning("loadResources() Exception: "+e.getMessage());
+				logger.warn("loadResources() Exception: "+e.getMessage());
 				e.printStackTrace();
 			}
 		}
-		log.info("loadResources() Loaded " + this.resources.size() + " resources");
+		logger.info("loadResources() Loaded " + this.resources.size() + " resources");
 	}
 
 	/**
