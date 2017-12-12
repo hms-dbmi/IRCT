c723d2e27189fb4d3dda08c00acd30a6094ea239
Yu Zhang <yu_zhang@hms.harvard.edu>
2017-12-12 14:39:56 -0500

    DI-994 initial code changes for whitelist functionality 1. configuration in standalone.xml 2. whitelist sample json file in resources folder 3. load in IRCTApplication.java 4. filter in SessionFilter

--- IRCT-API/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/util/Utilities.java
+++ IRCT-API/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/util/Utilities.java
@@ -7,6 +7,16 @@ import edu.harvard.hms.dbmi.bd2k.irct.event.IRCTEventListener;
 
 public class Utilities {
 	
+	public static class Naming {
+		
+		public static class Whitelist{
+			public static String WHITELIST_FALSE = "false";
+			public static String WHITELIST_JSON_NAME = "name";
+			public static String WHITELIST_JSON_RESOURCES = "resources";
+		}
+		
+	}
+	
 	public static IRCTEventListener getIRCTEventListener() {
 		try {
 			InitialContext ic = new InitialContext();
