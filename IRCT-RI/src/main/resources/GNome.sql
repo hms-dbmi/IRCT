f311cf559024b96b33a8752db9a61e5c05ccdbbf
Yu Zhang <yu_zhang@hms.harvard.edu>
2018-01-11 12:03:35 -0500

    DI-778 initial connection

--- IRCT-RI/src/main/resources/GNome.sql
+++ IRCT-RI/src/main/resources/GNome.sql
@@ -3,7 +3,7 @@ set @resourceName = '{{gnome_resource_name}}';
 set @resourceURL = '{{gnome_resource_url}}';
 set @gnomeUserName = '{{gnome_user_name}}';
 set @gnomePassword = '{{gnome_password}}';
-set @resourceImplementingInterface = 'edu.harvard.hms.dbmi.bd2k.irct.ri.gnome.GNome.GNomeResourceImplementation';
+set @resourceImplementingInterface = 'edu.harvard.hms.dbmi.bd2k.irct.ri.gnome.GNomeResourceImplementation';
 set @resourceOntology = 'TREE';
 
 -- SET THE RESOURCE VARIABLE
@@ -17,3 +17,13 @@ insert into `resource_parameters`(`id`, `name`, `value`) values(@resourceId, 're
 insert into `resource_parameters`(`id`, `name`, `value`) values(@resourceId, 'resourceURL', @resourceURL);
 insert into `resource_parameters`(`id`, `name`, `value`) values(@resourceId, 'gnomeUserName', @gnomeUserName);
 insert into `resource_parameters`(`id`, `name`, `value`) values(@resourceId, 'gnomePassword', @gnomePassword);
+
+-- INSERT THE RESOURCE DATATYPES
+insert into Resource_dataTypes(Resource_id, datatypes) values(@resourceId, 'edu.harvard.hms.dbmi.bd2k.irct.model.resource.PrimitiveDataType:STRING');
+
+-- SET THE FIELDS
+set @field_id = (select max(id) from Field) + 1;
+insert into Field(id, description, name, path, relationship, required) values(@field_id, '1st group of samples for comparison', 'project_type_A', null, null, 1);
+
+set @field_id = (select max(id) from Field) + 1;
+insert into Field(id, description, name, path, relationship, required) values(@field_id, '2st group of samples for comparison', 'project_type_B', null, null, 1);
