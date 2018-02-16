f99d368b99cca3c05bdb9aa50048e2148de658f4
Gabor Korodi <gabor_korodi@hms.harvard.edu>
2018-02-16 16:52:07 -0500

    Remove .sql scripts from PIC-SURE API repo
    Moved all .sql scripts to new public report: https://github.com/hms-dbmi/PIC-SURE-resources

--- IRCT-API/src/main/resources/add_dataconverters.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `DataConverterImplementation` (`id`, `dataConverter`, `format`, `resultDataType`)
-VALUES
-	(1, 'edu.harvard.hms.dbmi.bd2k.irct.ws.rs.resultconverter.JSONTabularDataConverter', 'JSON', 'TABULAR'),
-	(2, 'edu.harvard.hms.dbmi.bd2k.irct.ws.rs.resultconverter.XMLTabularDataConverter', 'XML', 'TABULAR'),
-	(3, 'edu.harvard.hms.dbmi.bd2k.irct.ws.rs.resultconverter.XSLXTabularDataConverter', 'XLSX', 'TABULAR'),
-	(4, 'edu.harvard.hms.dbmi.bd2k.irct.ws.rs.resultconverter.CSVTabularDataConverter', 'CSV', 'TABULAR');
--- IRCT-API/src/main/resources/sql_templates/i2b2passthrough_resource_delete.sql
+++
@@ -1,7 +0,0 @@
-set @resourceName = 'i2b2passthrough-i2b2-org';
-
-set @resourceId = (SELECT id FROM Resource WHERE name = @resourceName);
-
-DELETE FROM resource_parameter WHERE id = @resourceId;
-
-DELETE FROM resource WHERE name = @resourceName;
