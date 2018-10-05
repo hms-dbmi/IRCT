ad0bd3557b065bc4cb51f11ac67eace50f77136e
Mickaël Misbach <mickael@misba.ch>
2018-10-05 15:28:23 +0200

    add doc; change variable retrieval to avoid crash at startup

--- IRCT-CL/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/cl/filter/CORSFilter.java
+++ IRCT-CL/src/main/java/edu/harvard/hms/dbmi/bd2k/irct/cl/filter/CORSFilter.java
@@ -1,23 +1,65 @@
 package edu.harvard.hms.dbmi.bd2k.irct.cl.filter;
 
+import org.apache.log4j.Logger;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
 @Provider
+/**
+ * Filter adding support for the Cross-Origin Resource Sharing specification (https://www.w3.org/TR/cors/).
+ * Configuration with:
+ * - global/cors_enabled (boolean true / false)
+ * - global/cors_allow_origin (string for "Allow Origin" value)
+ */
 public class CORSFilter implements ContainerResponseFilter {
 
-    @javax.annotation.Resource(mappedName = "java:global/cors_allow_origin")
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
     private String corsAllowOrigin;
+    private Boolean corsEnabled;
+
+    /**
+     * Loads the CORS (Cross-Origin Resource Sharing) configuration.
+     */
+    private void loadConfig() {
+        if (corsEnabled != null) {
+            return;
+        }
+
+        try {
+            Context ctx = new InitialContext();
+            corsEnabled = (Boolean) ctx.lookup("global/cors_enabled");
+            corsAllowOrigin = (String) ctx.lookup("global/cors_allow_origin");
+            ctx.close();
+        } catch (NamingException e) {
+            logger.debug("CORS configuration NamingException, feature disabled", e);
+            corsEnabled = false;
+        }
+
+        if (!corsEnabled) {
+            logger.info("CORS is not enabled");
+        } else {
+            logger.info("CORS enabled, allow origin: " + this.corsAllowOrigin);
+        }
+    }
 
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) {
 
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", this.corsAllowOrigin);
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers","origin, content-type, accept, authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        this.loadConfig();
+
+        if (this.corsEnabled) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", this.corsAllowOrigin);
+            responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+            responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        }
     }
 }
